8d389b7923f52978330fe4c003fdcb5f2b17da03
Himani Bhatnagar <himanibhatnagar9311@gmail.com>
2026-04-04 21:01:38 +0530

    dashboard page

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,9 +1,45 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { ArrowLeftIcon } from "lucide-react";
 const ResumeBuilder = () => {
+  const { resumeId } = useParams();
+
+  const [resumeData, setResumeData] = useState({
+    _id: "",
+    title: "",
+    personal_info: {},
+    professioanl_summary: "",
+    experiece: [],
+    education: [],
+    projext: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3882F6",
+    public: false,
+  });
+
+  const loadExistingResume = async () => {
+    const resume = dummyResumeData.find((resume) => resume._id === resumeId);
+    if (resume) {
+      setResumeData(resume);
+      document.title = resume.title;
+    }
+  };
+
+  useEffect(() => {
+    loadExistingResume();
+  }, []);
+
   return (
     <div>
-      <h1>resume builder page</h1>
+      <div className="max-w-7xl mx-auto px-4 py-6">
+        <Link
+          to={"/app"}
+          className="inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all"
+        >
+          <ArrowLeftIcon className="size-4" /> Back to Dashboard
+        </Link>
+      </div>
     </div>
   );
 };

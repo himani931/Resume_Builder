b5dedc6f0e0e45bd26491eecf4b03ab748710848
Himani Bhatnagar <himanibhatnagar9311@gmail.com>
2026-07-20 18:27:43 +0530

    Add User model

---
+++ server/controllers/userController.js
@@ -0,0 +1,99 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  return token;
+};
+
+// controller for user registration
+//Post : /api/users/register
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    //check if required fields are present
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    //check if user alrady exists
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    //create new user
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    //return success message
+    const token = generateToken(newUser._id);
+    newUser.password = undefined;
+
+    return res
+      .status(201)
+      .json({ message: "User created successfully", token, user: newUser });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// controller for user login
+//Post : /api/users/login
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //check if user exists
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    //check if password is correct
+    if (!user.comparePassword(password)) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    //return success message
+    const token = generateToken(user._id);
+    user.password = undefined;
+
+    return res
+      .status(200)
+      .json({ message: "User login successfully", token, user });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+//controller for getting user by id
+//GET: /api/user/data
+
+export const getUserById = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    //checlk if user exist
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.password = undefined;
+    return res.status(201).json({ user });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  getUserById,
+  loginUser,
+  registerUser,
+} from "../controllers/userController.js";
+import protect from "../middelwares/authMiddelware.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/register", registerUser);
+
+userRouter.post("/login", loginUser);
+
+userRouter.get("/data", protect, getUserById);
+
+export default userRouter;
